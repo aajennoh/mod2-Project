8c4770d2c2ba182f3a4ab2765ce3d73be0606674
Kirbyasdf <kirbyAugustus@gmail.com>
2019-05-28 23:44:18 -0400

    blinged it out

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -51,7 +51,7 @@ class RecipesController < ApplicationController
 private
 
 def recipe_params
-params.require(:recipe).permit(:name, :description, :total_price, :total_protein, :total_calories, :user_id, food_ids:[])
+params.require(:recipe).permit(:name, :description, :total_price, :total_protein, :total_calories, :user_id, food_ids:[], foods_attributes: [:id, :name, :_destroy])
 end
 
 
