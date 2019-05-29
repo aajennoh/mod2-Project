5f560c7e94531d999b83467dfc76cb7deffde591
Jennifer Oh <ohjennifer.90@gmail.com>
2019-05-29 10:54:37 -0400

    changes to login

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
 resources :foods
 resources :recipes
 resources :users
-
-get "/welcome", to: "users#welcome"
-
-
-
-
-
 resources :recipe_foods
 
+
+get "/welcome", to: "users#welcome"
 get '/login', to: 'sessions#new', as: 'login'
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy', as: 'logout'
